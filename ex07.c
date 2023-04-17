41008eff654c4d2792714237559d89e038a7e3f6
Victória Silva <120598131+Victoriasilvagit@users.noreply.github.com>
2023-04-17 12:35:08 -0300

    Add files via upload

--- ex07.c
+++ ex07.c
@@ -1,32 +1,33 @@
-/*7) Fazer um programa para ler o salário de uma pessoa, o percentual de aumento e o percentual de
-descontos. Os descontos incidem sobre o salário com aumento. Calcular o novo salário e mostrá-lo como no
+/*7) Fazer um programa para ler o salï¿½rio de uma pessoa, o percentual de aumento e o percentual de
+descontos. Os descontos incidem sobre o salï¿½rio com aumento. Calcular o novo salï¿½rio e mostrï¿½-lo como no
 exemplo a seguir.*/
 #include <stdio.h>
+#include <math.h>
 
 int main()
 {
 	double percentualAumento, percentualDesconto, salario, centavos;
 	int aumento, desconto, reais;
 
-	
+
 	//entrada
 	printf("Informe o salario:");
 	scanf("%lf", &salario);
-	printf("Informe o percentual de aumento: (Ex: 10 para 10%)");
+	printf("Informe o percentual de aumento: (Ex: 10 para 10)");
 	scanf("%d", &aumento);
-	printf("Informe o percentual de desconto:(Ex: 3 para 3%)");
+	printf("Informe o percentual de desconto:(Ex: 3 para 3)");
 	scanf("%d", &desconto);
-	
-	//processamento 
+
+	//processamento
 	percentualAumento = (salario * aumento / 100) + salario;
 	percentualDesconto =  percentualAumento - (percentualAumento * desconto / 100);
 	reais = round(percentualDesconto);
-	centavos = (percentualDesconto - reais) * 100;
+	centavos = (percentualDesconto - reais) * 100 ;
 	//saida
 	printf("Salario aumentado: %.2lf\n", percentualAumento);
 	printf("Salario liquido: %.2lf\n", percentualDesconto);
-	printf("O salario liquido eh: %d reais e %d centavos", reais, centavos);
-	
+	printf("O salario liquido eh: %d reais e %.0lf centavos", reais, centavos);
+
 	return 0;
-	
+
 }
