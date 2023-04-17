2637c7957bac4763991998787a481b116f2f7e8d
Victória Silva <victoriadoesantosilva@gmail.com>
2023-04-17 09:54:33 -0300

    Create ex07.c

---
+++ ex07.c
@@ -0,0 +1,32 @@
+/*7) Fazer um programa para ler o salário de uma pessoa, o percentual de aumento e o percentual de
+descontos. Os descontos incidem sobre o salário com aumento. Calcular o novo salário e mostrá-lo como no
+exemplo a seguir.*/
+#include <stdio.h>
+
+int main()
+{
+	double percentualAumento, percentualDesconto, salario, centavos;
+	int aumento, desconto, reais;
+
+	
+	//entrada
+	printf("Informe o salario:");
+	scanf("%lf", &salario);
+	printf("Informe o percentual de aumento: (Ex: 10 para 10%)");
+	scanf("%d", &aumento);
+	printf("Informe o percentual de desconto:(Ex: 3 para 3%)");
+	scanf("%d", &desconto);
+	
+	//processamento 
+	percentualAumento = (salario * aumento / 100) + salario;
+	percentualDesconto =  percentualAumento - (percentualAumento * desconto / 100);
+	reais = round(percentualDesconto);
+	centavos = (percentualDesconto - reais) * 100;
+	//saida
+	printf("Salario aumentado: %.2lf\n", percentualAumento);
+	printf("Salario liquido: %.2lf\n", percentualDesconto);
+	printf("O salario liquido eh: %d reais e %d centavos", reais, centavos);
+	
+	return 0;
+	
+}
