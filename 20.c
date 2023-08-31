228ae8bac86213d39cb29bf7e36845599d073865
Victória Silva <120598131+Victoriasilvagit@users.noreply.github.com>
2023-08-31 08:56:56 -0300

    Update and rename senha autorizada.c to 20.c

--- 20.c
+++ 20.c
@@ -1,5 +1,5 @@
-/*1) Elaborar um programa que leia um n˙mero que representa uma senha e verifica se a mesma est· correta
-ou n„o, comparando-a com 12345 e informa "Acesso autorizado" ou "Acesso negado", conforme o caso.*/
+/*1) Elaborar um programa que leia um n√∫mero que representa uma senha e verifica se a mesma est√° correta
+ou n√£o, comparando-a com 12345 e informa "Acesso autorizado" ou "Acesso negado", conforme o caso.*/
 #include <stdio.h>
 int main()
 {
