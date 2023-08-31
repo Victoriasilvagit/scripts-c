fb70d19bf376e6cefbc2acd84b5d997747eb8727
Victória Silva <120598131+Victoriasilvagit@users.noreply.github.com>
2023-08-31 08:59:40 -0300

    Update and rename divisível.c to 24.c

--- 24.c
+++ 24.c
@@ -1,14 +1,14 @@
-/*9) Ler um número e utilizando uma estrutura if else if else if... (obrigatoriamente encadeada) informar se ele:
-a) É divisível por 5, por 3 ou por 2;
-Exemplo: 30 é divisível por 2, 3 e 5.
-b) É divisível somente por 5 e por 3; por 5 e por 2; ou por 3 e por 2;
-Exemplo: 15 é divisível somente por 3 e por 5.
-Exemplo: 10 é divisível somente por 5 e por 2.
-Exemplo: 6 é divisível somente por 3 e por 2.
-c) É divisível somente por 5, por 3 ou por 2;
-Exemplo: 25 é divisível somente por 5
-d) Não é divisível por nenhum destes;
-Exemplo: 7 não é divisível por 5, por 3 ou por 2;*/
+/*9) Ler um nÃºmero e utilizando uma estrutura if else if else if... (obrigatoriamente encadeada) informar se ele:
+a) Ã‰ divisÃ­vel por 5, por 3 ou por 2;
+Exemplo: 30 Ã© divisÃ­vel por 2, 3 e 5.
+b) Ã‰ divisÃ­vel somente por 5 e por 3; por 5 e por 2; ou por 3 e por 2;
+Exemplo: 15 Ã© divisÃ­vel somente por 3 e por 5.
+Exemplo: 10 Ã© divisÃ­vel somente por 5 e por 2.
+Exemplo: 6 Ã© divisÃ­vel somente por 3 e por 2.
+c) Ã‰ divisÃ­vel somente por 5, por 3 ou por 2;
+Exemplo: 25 Ã© divisÃ­vel somente por 5
+d) NÃ£o Ã© divisÃ­vel por nenhum destes;
+Exemplo: 7 nÃ£o Ã© divisÃ­vel por 5, por 3 ou por 2;*/
 #include <stdio.h>
 
 int main() 
