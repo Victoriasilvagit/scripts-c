836f2e66b965471a3b128df12441cff2ffd304cb
Victória Silva <120598131+Victoriasilvagit@users.noreply.github.com>
2023-08-31 08:57:30 -0300

    Rename ex02.c to 02.c

--- 02.c
+++ 02.c
@@ -1,5 +1,5 @@
-/*2) Considerando que para um consórcio sabe-se o número total de prestações, a quantidade de prestações
-pagas e o valor de cada prestação (que é fixo). Escreva um programa que determine o valor total já pago pelo
+/*2) Considerando que para um consÃ³rcio sabe-se o nÃºmero total de prestaÃ§Ãµes, a quantidade de prestaÃ§Ãµes
+pagas e o valor de cada prestaÃ§Ã£o (que Ã© fixo). Escreva um programa que determine o valor total jÃ¡ pago pelo
 consorciado e o saldo devedor.*/
 
 #include <stdio.h>
